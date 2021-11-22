c271bc32c80262297db2d4cb859fb61ab1a5a11e
MikheilBregvadze <mikheil.bregvadze@geolab.edu.ge>
2021-11-22 17:44:33 +0400

    add router

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Router from './pages/desktop/layout/routes/routes';
 import Header from './pages/desktop/layout/header/header';
+
 import './index.css'
+
 function App() {
   return (
     <div className="App">
       <Header/>
+      <section>
+        <Router />
+      </section>
     </div>
   );
 }
