62c1b0b7ddd0bbed7a1a42ca3a25902d7a8ab5a7
d.dekanosidze <dd.dekanosidze@gmail.com>
2021-12-06 10:26:23 +0400

    ...

--- clientapp/src/pages/desktop/profile/balance/Balance.jsx
+++ clientapp/src/pages/desktop/profile/balance/Balance.jsx
@@ -4,6 +4,8 @@ import '../../../../assets/images/bank.png'
 function AddBalance() {
     return (
         <div className={style.addBalanceContainer}>
+            <div className={style.deposit} >Deposit  |  Withdrawal</div>
+            <div className={style.logo} >You have not linked any card yet Save the card on funds deposit. Save time using linked cards in future.</div>
         </div>
     )
 }
