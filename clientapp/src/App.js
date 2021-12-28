769ebb52c7a26b39e8c5c9048fd9ece43d58bdd9
MikheilBregvadze <mikheil.bregvadze@geolab.edu.ge>
2021-12-28 18:37:16 +0400

    add GA

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Router from './pages/desktop/layout/routes/Routes';
 import Header from './pages/desktop/layout/header/Header';
 import { Auth, AuthContext } from './services/context/AuthContext';
+import { initGA } from '.';
 
 import './index.css'
 
 function App() {
   const auth = AuthContext();
+  useEffect(() => { initGA(); }, []);
   return (
     <Auth.Provider value={auth}>
       <div className="App">
--- clientapp/src/index.js
+++ clientapp/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import App from './App';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
+
+export const initGA = () => {       
+  ReactGA.initialize('G-5MH2XEDC0N'); 
+} 
+
+export const GApageView = (page) => {   
+  ReactGA.pageview(page);   
+}
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
