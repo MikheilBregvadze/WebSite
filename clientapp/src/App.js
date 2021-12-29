87542ed75728bfdb59783c7986137cec0154d905
MikheilBregvadze <mikheil.bregvadze@geolab.edu.ge>
2021-12-29 12:41:29 +0400

    ...

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TagManager from 'react-gtm-module'
 import Router from './pages/desktop/layout/routes/Routes';
 import Header from './pages/desktop/layout/header/Header';
+import { initGA } from '.';
 import { Auth, AuthContext } from './services/context/AuthContext';
 
 import './index.css'
 
 function App() {
   const auth = AuthContext();
+  useEffect(() => { initGA(); }, []);
   const tagManagerArgs = {
       gtmId: 'GTM-58MFBK7'
   };
