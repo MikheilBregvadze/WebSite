923b98077c152c3cc94988c77fb4365a4101d0e3
MikheilBregvadze <mikheil.bregvadze@geolab.edu.ge>
2021-12-29 10:14:47 +0400

    ...

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Router from './pages/desktop/layout/routes/Routes';
 import Header from './pages/desktop/layout/header/Header';
+import { initGA } from '.';
 import { Auth, AuthContext } from './services/context/AuthContext';
 
 import './index.css'
 
 function App() {
   const auth = AuthContext();
+  useEffect(() => { initGA(); }, []);
   return (
     <Auth.Provider value={auth}>
       <div className="App">
